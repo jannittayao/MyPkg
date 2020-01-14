09afb0577e8ccbd321d14ce9d2a2ba43b83581b1
Jannitta Yao <jyao@wellesley.edu>
2020-01-14 13:26:11 -0500

    Initialize Git repo for test package

---
+++ docs/make.jl
@@ -0,0 +1,4 @@
+push!(LOAD_PATH,"/Users/jannitta/.julia/dev/MyPkg/src/")
+using Documenter, MyPkg
+
+makedocs(sitename="My Documentation")
--- src/MyPkg.jl
+++ src/MyPkg.jl
@@ -1,5 +1,23 @@
 module MyPkg
 
-greet() = print("Hello World!")
+export testHelloWorld, addOne
+
+"""
+    testHelloWorld()
+
+Prints 'Hello World!'
+"""
+function testHelloWorld()
+    return "Hello World!"
+end
+
+"""
+    addOne(int)
+
+Adds one to a given integer
+"""
+function addOne(n)
+    return n+1
+end
 
 end # module
